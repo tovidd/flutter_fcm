6b1287c3b974b5f2094cdb0ce6b86ee2a2a04c08
tovidd <toviddd@gmail.com>
2020-10-02 11:01:27 +0700

    Fix unable install, caused by PluginRegistry
    Error "GeneratedPluginRegistrant.registerWith(registry)"

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # flutter_fcm
 
-A new Flutter project.
+dibuat untuk mempelajari firebase cloud messaging, tetapi karena malas bikin projek baru, contoh lain pun di masukkan sana sini sehingga aplikasi agak berantakan.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+urutan contoh yang ada diaplikasi:
+- FCM
--- android/app/src/main/java/io/flutter/plugins/Application.java
+++ android/app/src/main/java/io/flutter/plugins/Application.java
@@ -1,10 +1,12 @@
 package io.flutter.plugins;
 
  import io.flutter.app.FlutterApplication;
+ import io.flutter.embedding.engine.FlutterEngine;
  import io.flutter.plugin.common.PluginRegistry;
  import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
  import io.flutter.plugins.GeneratedPluginRegistrant;
  import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+ import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
 
  public class Application extends FlutterApplication implements PluginRegistrantCallback {
    @Override
@@ -15,6 +17,7 @@ package io.flutter.plugins;
 
    @Override
    public void registerWith(PluginRegistry registry) {
-     GeneratedPluginRegistrant.registerWith(registry);
+    //  GeneratedPluginRegistrant.registerWith(registry);
+    FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
    }
  }
